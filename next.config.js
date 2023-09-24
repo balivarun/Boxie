f8f06b6b706e56e71075e505c74a0975467a41cc
varun <varunbali345@gmail.com>
2023-09-24 10:06:05 +0530

    added fabricjs and created working code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+
+module.exports = {
+    webpack: (
+      config,
+      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+      // Important: return the modified config
+      config.externals.push({ sharp: 'commonjs sharp', canvas: 'commonjs canvas' })
+      return config
+    },
+  }
